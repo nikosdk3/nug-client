b927f8fb43bcc34f97a6a63765836625c58429ed
Nikos <nikomvp03@gmail.com>
2023-09-04 19:49:44 -0500

    Tracers now works with storage blocks

--- src/main/java/nikosdk3/nugclient/modules/render/Tracers.java
+++ src/main/java/nikosdk3/nugclient/modules/render/Tracers.java
@@ -1,7 +1,12 @@
 package nikosdk3.nugclient.modules.render;
 
 import com.google.common.eventbus.Subscribe;
+import net.minecraft.block.entity.BarrelBlockEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import nikosdk3.nugclient.events.event.RenderEvent;
 import nikosdk3.nugclient.modules.Category;
@@ -12,6 +17,7 @@ import nikosdk3.nugclient.settings.Setting;
 import nikosdk3.nugclient.utils.Color;
 import nikosdk3.nugclient.utils.EntityUtils;
 import nikosdk3.nugclient.utils.RenderUtils;
+import nikosdk3.nugclient.utils.Utils;
 
 public class Tracers extends Module {
     private final Setting<Color> color = addSetting(new ColorSetting.Builder()
@@ -42,27 +48,43 @@ public class Tracers extends Module {
             .build()
     );
 
+    private final Setting<Boolean> storage = addSetting(new BoolSetting.Builder()
+            .name("storage")
+            .description("Trace storage blocks.")
+            .defaultValue(false)
+            .build()
+    );
+
+    private Vec3d vec1;
+
     public Tracers() {
         super(Category.Render, "tracers", "Renders lines to entities.");
     }
 
     public void render(Entity entity) {
-        Vec3d vec1 = new Vec3d(0, 0, 100)
-                .rotateX(-(float) Math.toRadians(mc.player.getPitch()))
-                .rotateY(-(float) Math.toRadians(mc.player.getYaw()))
-                .add(mc.cameraEntity.getPos()
-                        .add(0, mc.cameraEntity.getEyeHeight(mc.cameraEntity.getPose()), 0));
         Vec3d vec2 = entity.getPos().add(0, entity.getEyeHeight(entity.getPose()), 0);
-
         RenderUtils.line(vec1.x, vec1.y, vec1.z, vec2.x, vec2.y, vec2.z, color.get());
     }
 
     @Subscribe
     public void onRender(RenderEvent event) {
+        vec1 = new Vec3d(0, 0, 100)
+                .rotateX(-(float) Math.toRadians(mc.player.getPitch()))
+                .rotateY(-(float) Math.toRadians(mc.player.getYaw()))
+                .add(mc.cameraEntity.getPos()
+                        .add(0, mc.cameraEntity.getEyeHeight(mc.cameraEntity.getPose()), 0));
         for (Entity entity : mc.world.getEntities()) {
             if (players.get() && EntityUtils.isPlayer(entity) && entity != mc.player) render(entity);
             if (mobs.get() && EntityUtils.isMob(entity)) render(entity);
             if (animals.get() && EntityUtils.isAnimal(entity)) render(entity);
+            if (storage.get()) {
+                for (BlockEntity blockEntity : Utils.blockEntities()) {
+                    if (blockEntity instanceof ChestBlockEntity || blockEntity instanceof ShulkerBoxBlockEntity || blockEntity instanceof BarrelBlockEntity) {
+                        BlockPos pos = blockEntity.getPos();
+                        RenderUtils.line(vec1.x, vec1.y, vec1.z, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, color.get());
+                    }
+                }
+            }
         }
     }
 }
